b41e2fd28227eec9a567578347ba466710b59c38
Sunday Ochigbo <131619007+sunday004@users.noreply.github.com>
2024-08-11 21:22:37 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD6lD6DbWmXKCyfgV_cjVLKD6dd0oha598",
+  authDomain: "inventory-management-79af3.firebaseapp.com",
+  projectId: "inventory-management-79af3",
+  storageBucket: "inventory-management-79af3.appspot.com",
+  messagingSenderId: "344980353682",
+  appId: "1:344980353682:web:3a2e5829d77d0d267eac4b",
+  measurementId: "G-1LQFK1F130"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export {firestore}
